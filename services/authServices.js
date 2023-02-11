8f5c81300912202a6b399ca9fc65399322561feb
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2023-02-11 23:18:38 +0200

    check if user exists

--- services/authServices.js
+++ services/authServices.js
@@ -2,14 +2,21 @@ const User = require('../models/User.js');
 
 const bcrypt = require('bcrypt');
 
+exports.findByUsername = (username) => User.findOne(username);//User.exists(username)
+
 exports.register = async (username, email, password, confirmPassword) => {
 
     if (password !== confirmPassword) {
         throw new Error('Password missmatc!');
     }
 
-    //TODO:Validate password колко дълга ,симшоли език,гл .букви
+    //TODO:Check user exists
+    const existingUser = await this.findByUsername(username);
+    if (existingUser) {
+        throw new Error('User  exists!');
+    }
 
+    //TODO:Validate password колко дълга ,симшоли език,гл .букви
     if (password.lenght < 4) {
         throw new Error('The password should be at least four characters long!');
     }

95d8ca18c5b77d5a811e013e4e5441122212fdd0
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2023-02-11 12:58:22 +0200

    Install mongoose

--- Skelet/index.js
+++ Skelet/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
+const mongoose = require('mongoose');
 
 const routes = require('./routes.js');
 
@@ -9,12 +10,14 @@ app.engine('hbs', handlebars.engine({
     extname: 'hbs',
 }));
 
-app.set('view engine','hbs')
+app.set('view engine', 'hbs')
 
 
-app.use('/static',express.static('public'));
+app.use('/static', express.static('public'));
 app.use(express.urlencoded({ extended: false }));//add body parser
 app.use(routes);
 
+//TODO:change database name
+mongoose.connect(`mongodb://127.0.0.1:27017/crypto`);
 
 app.listen(5000, () => console.log('Server is running on port 5000...'))

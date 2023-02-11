a73f4cafe5f98931b7a38b2a90f57ffb89611c06
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2023-02-11 22:14:37 +0200

    Add Crypto Model

--- models/Crypto.js
+++ models/Crypto.js
@@ -1,7 +1,29 @@
 const mongoose = require('mongoose');
 
-
 const cryptoShema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    image:
+    {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    cryptoDescription: {
+        type: String,
+        required: true,
+    },
+    paymentMethod: {
+        type: String,
+        required: true,
+    } //String(crypto - wallet, credit - card, debit - card, paypal) required,
+    // buyaCrypto - a collection of Users(a reference to the User model)
+    // owner - object Id(a reference to the User model)
 
 });
 

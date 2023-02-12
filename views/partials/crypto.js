f337b9b57b2714e33d3b66286ab6c31fd3b66bfa
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2023-02-12 18:45:02 +0200

    Update crypto partials and catalog page

--- views/partials/crypto.js
+++
@@ -1,6 +0,0 @@
-<div class="coin-text">
-<h1 id="name">{{name}}</h1>
-<h3 id="payment">Payment method: {{payment}}</h3>
-<p id="price"><span>Price: ${{price}}</span></p>
-<p id="description">{{description}}</p>
-</div>

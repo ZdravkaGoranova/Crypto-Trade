2c100928565d2134691f7fc680fb689fe0f7dde5
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2023-02-12 17:09:19 +0200

    Add crypto partials

---
+++ views/partials/crypto.js
@@ -0,0 +1,6 @@
+<div class="coin-text">
+<h1 id="name">{{name}}</h1>
+<h3 id="payment">Payment method: {{payment}}</h3>
+<p id="price"><span>Price: ${{price}}</span></p>
+<p id="description">{{description}}</p>
+</div>

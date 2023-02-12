72c79e4226f0be92582605d0cb08b44c7af75cf8
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2023-02-12 16:08:48 +0200

    Update errorUtils

--- utils/errorUtils.js
+++ utils/errorUtils.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 function getFirstMongooseError(error) {
 
     const errors = Object.keys(error.errors).map(key => error.errors[key].message);
+    //или  const firstError = Object.values(error.errors)[0].message;
     //масив от стрингове на message на всички грешки
-    return errors[0];
+    return errors[0];//    return firstError;
 };
 
 exports.getErrorMessage = (error) => {
